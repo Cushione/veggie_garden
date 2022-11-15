07c10ecbe4baed2f7fd3cc7f19c0b5a8e725df6a
Cushione <maulerovak@gmail.com>
2022-11-15 15:01:34 +0000

    Add prepare next season function

--- src/player.py
+++ src/player.py
@@ -3,6 +3,7 @@ from .storage import Storage
 from .fertiliser import Fertiliser
 from .field import Field
 from .store import Store
+from .utils import valid_number_input
 
 
 class Player():
@@ -13,3 +14,26 @@ class Player():
         self.fertiliser = Fertiliser(0)
         self.fields = [Field(None, 0, self.storage)]
         self.store = Store()
+    def prepare_next_season(self):
+        while True:
+            print("1: Open Storage")
+            print("2: Fertiliser")
+            print("3: Visit Store")
+            print("4: Go to Fields")
+            print("5: Start season")
+            print("0: Save and Exit")
+            user_input = valid_number_input("What would you like to do next?: ", 0, 5)
+            
+            if user_input == 1:
+                # TODO: Open Storage
+            elif user_input == 2:
+                # TODO: Open Fertiliser
+            elif user_input == 3:
+                self.store.buy_seeds(self)
+            elif user_input == 4:
+                # TODO: Open Fields
+            elif user_input == 5:
+                # TODO: Start season
+            else:
+                # TODO: Save Game and show main screen
+
