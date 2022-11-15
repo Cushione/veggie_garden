2cc7bdfa680ed4aa7a007fac0af960813547c3ce
Cushione <maulerovak@gmail.com>
2022-11-15 10:16:45 +0000

    Add crops enum to utils

--- src/storage.py
+++ src/storage.py
@@ -1,13 +1,11 @@
-from .utils import SHEET
+from .utils import CROPS
 
 class Storage():
     def __init__(self, seeds):
         self.seeds = self.init_seeds(seeds)
 
     def init_seeds(self, seeds):
-        crops_sheet = SHEET.worksheet("crops")
-        crops_data = crops_sheet.col_values(1)
-        return dict(zip(crops_data[1:], seeds))
+        return dict(zip([crop.name for crop in CROPS], seeds))
 
     def available_seeds(self, crop):
         return self.seeds[crop]
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from enum import Enum
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -11,3 +12,8 @@ CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("veggie_garden")
+
+
+crops_sheet = SHEET.worksheet("crops")
+crop_names = crops_sheet.col_values(1)
+CROPS = Enum("CROPS", crop_names)
