f53d39980e5f0aa11a226c8bd73124f310bad328
Cushione <maulerovak@gmail.com>
2022-11-17 09:59:25 +0000

    Add new page function
    - Clears terminal and prints header

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 from enum import Enum
+import os
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -57,3 +58,17 @@ def valid_confirm_input(prompt):
 
 def press_enter():
     input("Press Enter to continue.")
+
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def new_page(game):
+    clear_terminal()
+    username = f" - {game.username}" if game else ""
+    game_id = f"ID: {game.id}" if game else ""
+    season = f"Year {int((game.player.month / 6) + 1)}" if game else "" 
+    money = f"€{game.player.money}" if game else ""
+    print(f"Veggie Garden {game_id.ljust(9)}{username.ljust(15)} {money.rjust(27)} {season.rjust(9)}")
+    print("-----------------------------------------------------------------------------\n")
